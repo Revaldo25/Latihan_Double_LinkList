cd62e95a16898662af12d79d80fbffaff4939abe
Revaldo <113174251+Revaldo25@users.noreply.github.com>
2024-05-21 17:37:32 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,74 +1,73 @@
-#include <iostream>
 #include "header.h"
 
-using namespace std;
-
-void displayMenu() {
-    cout << "----------------------\n";
-    cout << "OPERASI PADA DOUBLE LINKED LIST\n";
-    cout << "----------------------\n";
-    cout << "[1] Tambah data dari depan\n";
-    cout << "[2] Tambah data dari belakang\n";
-    cout << "[3] Hapus data dari depan\n";
-    cout << "[4] Hapus data dari belakang\n";
-    cout << "[5] Cetak Data\n";
-    cout << "[6] Clear\n";
-    cout << "[7] Tampil Berurut Dari Belakang node\n";
-    cout << "[8] Hitung Jumlah Node\n";
-    cout << "[0] Keluar\n";
-    cout << "----------------------\n";
-    cout << "\nMasukkan pilihan: ";
-}
-
 int main() {
-    DoubleLinkedList dll;
-    int choice, data;
-
-    while (true) {
-        displayMenu();
-        cin >> choice;
-
-        switch (choice) {
+    int databaru;
+    head baru = NULL;
+    
+    int choose;
+    do {
+       cout << "---------------------------\n"
+             << "      OPERASI PADA DOUBLE LINKED LIST\n"
+             << "---------------------------\n"
+             << "[1] Tambah data dari depan \n"
+             << "[2] Tambah data dari belakang\n"
+             << "[3] Hapus data dari depan \n"
+             << "[4] Hapus data dari belakang \n"
+             << "[5] Cetak data \n"
+             << "[6] Clear\n"
+             << "[7] Tampil Berurut Dari Belakang\n"
+             << "[8] Hitung Jumlah Node\n"
+             << "[0] Keluar\n"
+             << "---------------------------\n"
+             << "Masukkan pilihan : "; 
+        cin >> choose;
+        
+        switch (choose) {
             case 1:
-                cout << "\nMasukkan data: ";
-                cin >> data;
-                dll.prepend(data);
+                cout << "Penyisipan Simpul Di Depan" << endl << endl;
+                cout << "Masukkan angka :"; 
+                cin >> databaru;
+                insertDepan(baru, databaru);
+                cout << endl;
+                tampil(baru);
                 break;
             case 2:
-                cout << "\nMasukkan data: ";
-                cin >> data;
-                dll.append(data);
+                cout << "Penyisipan Simpul Di Belakang" << endl << endl;
+                cout << "Masukkan angka :"; 
+                cin >> databaru;
+                insertBelakang(baru, databaru);
+                cout << endl;
+                tampil(baru);
                 break;
             case 3:
-                dll.deleteFront();
-                cout << "\nNode di depan telah dihapus." << endl;
+                hapusDepan(baru);
+                tampil(baru);
                 break;
             case 4:
-                dll.deleteBack();
-                cout << "\nNode di belakang telah dihapus." << endl;
+                hapusBelakang(baru);
+                tampil(baru);
                 break;
             case 5:
-                cout << "\nIsi linked list: ";
-                dll.display();
+                tampil(baru);
                 break;
             case 6:
-                dll.clear();
-               cout << "\nSemua node telah dihapus." << endl;
+                clear(baru);
                 break;
             case 7:
-                cout << "\nIsi linked list dari belakang: ";
-                dll.displayFromTail();
+                tampilReverse(baru);
                 break;
             case 8:
-                cout << "\nJumlah node: " << dll.countNodes() << endl;
+                cout << "Jumlah nodes: " << hitungNode(baru) << endl;
                 break;
             case 0:
-                cout << "\nKeluar program." << endl;
-                return 0;
+                cout << "Keluar" << endl;
+                break;
             default:
-                cout << "\nPilihan tidak valid." << endl;
+                cout << "Pilihan tidak tersedia" << endl;
+                break;
         }
-    }
-
+    } while (choose != 0);
     return 0;
 }
+
+
